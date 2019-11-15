77bd2dd19812781ab43d793b8a9a3d755c0628d3
Leslie Harvey <lharvey427@gmail.com>
2019-11-14 23:21:33 -0500

    implement data changes into a function

--- DataExtractor.py
+++ DataExtractor.py
@@ -14,23 +14,33 @@ import pandas as pd
 
 # --------------------------------------------------------------------
 file = pd.read_excel(r'/Users/leslieharvey/Desktop/TestFile.xlsx')
-for col in file:
-    if col == "section":
-        for index, row in file[col].iteritems():
-            # Deletes the students not in the desired section
-            if file[col][index] != "COP2271-29AD(13148)":
-                file = file.drop(index)
-            # Only keeps the students' first attempt
-            else:
-                if file["attempt"][index] != 1:
-                    file = file.drop(index)
+currentSection = "COP2271-29AD(13148)"
 
-# Sorts the values according to their student id
-file = file.sort_values(['sis_id'], ascending=True)
 
-# Deletes the columns not needed
-for col in file:
-    if (col != "name") and (col != "sis_id") and (col != "submitted"):
-        file = file.drop(columns=col)
+# Created function to process data changes
+def modify_file(section, df):
+    for col in df:
+        if col == "section":
+            for index, row in df[col].iteritems():
+                # Deletes the students not in the desired section
+                if df[col][index] != section:
+                    df = df.drop(index)
+                # Only keeps the students' first attempt
+                else:
+                    if df["attempt"][index] != 1:
+                        df = df.drop(index)
+
+    # Sorts the values according to their student id
+    df = df.sort_values(['sis_id'], ascending=True)
+
+    # Deletes the columns not needed
+    for col in df:
+        if (col != "name") and (col != "sis_id") and (col != "submitted"):
+            df = df.drop(columns=col)
+
+    return df
+
+
+file = modify_file(currentSection, file)
 
 file.to_excel("output.xlsx")
