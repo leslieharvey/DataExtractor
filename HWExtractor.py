d42a246cd5211ff218f76cf9fe15514a4139aa94
Leslie Harvey <lharvey427@gmail.com>
2019-11-18 23:45:45 -0500

    enhanced ability to process data

--- HWExtractor.py
+++ HWExtractor.py
@@ -3,10 +3,11 @@ import datetime
 import xlsxwriter
 
 # Details needed to access Canvas and the course
-API_URL = 'https://ufl.test.instructure.com/'
+API_URL = 'https://ufl.instructure.com/'
 API_KEY = '1016~40RIzLLTgT01gRVzfhbsvMFIPWWZZZY4KhPF3WfAiPWhdv9Gi2HAZMarQ6uR8oAR'
 courseID = 378337
 section_num = ["000.UFL.2019-08-UF-0.13148"]
+section_ID = "COP2271-29AD(13148)"
 
 # Initialize a new Canvas object
 canvas = Canvas(API_URL, API_KEY)
@@ -15,12 +16,18 @@ course = canvas.get_course(courseID)
 
 # Get students for the specified section
 enrollments = course.get_enrollments(type=["StudentEnrollment"], sis_section_id=section_num)
-# Build a map of student ID to their name
-idToName = dict((e.sis_user_id, e.user['name']) for e in enrollments)
-# Build a map of Canvas ID to their student ID using a nested dictionary
-idToCanvas = {}
+
+# Build a map of Canvas ID to their student ID using a nested dictionary for HW
+idHW = {}
 for e in enrollments:
-    idToCanvas[e.user['id']] = {'canvas': e.user['id'], 'UF': e.sis_user_id, 'name': e.user['name']}
+    idHW[e.user['id']] = {'canvas': e.user['id'], 'UF': e.sis_user_id, 'name': e.user['name']}
+assignmentsHW = course.get_assignments(includes=['overrides'], search_term="HW:")
+
+# Build a map of Canvas ID to their student ID using a nested dictionary for Quizzes
+idQuiz = {}
+for e in enrollments:
+    idQuiz[e.user['id']] = {'canvas': e.user['id'], 'UF': e.sis_user_id, 'name': e.user['name']}
+assignmentsQuiz = course.get_assignments(includes=['overrides'], search_term="Quiz")
 
 # # Need to figure out how to get past attempt history
 # assignment = course.get_assignment(3970170)
@@ -28,71 +35,90 @@ for e in enrollments:
 #
 # print(type(submissions.submission_history))
 
-# Searching for all of the homework assignments
-assignments = course.get_assignments(search_term="HW:")
-for a in assignments:
-    print(a.name)
-    # Due date of the assignment
-    due_date = datetime.datetime.strptime(a.due_at, "%Y-%m-%dT%H:%M:%SZ")
-    for s in a.get_submissions(include=["submission_history"]):
-        try:
-            # Only acting on students in specified section
-            for person in idToCanvas:
-                if s.user_id == person:
-                    sub_date = datetime.datetime.strptime(s.submitted_at, "%Y-%m-%dT%H:%M:%SZ")
-                    duration = due_date - sub_date
-                    duration_seconds = duration.total_seconds()
-                    hours = duration_seconds / 3600
-                    idToCanvas[person].update({a.name: str(hours)})
-                    # print(str(hours) + "   " + str(s.user_id))
-                    # if s.user_id == 1027771:
-                        # print(due_date)
-                        # print(sub_date)
-                        # print(duration)
-                        # print(duration_seconds)
-                        # print(hours)
-        except:
-            print("NaN for: " + str(person))
-    print("\n")
 
 # Compile all the information into an excel workbook
 # ----------------------------------------------------
 # Creating the file
 data = xlsxwriter.Workbook("ExportedData.xlsx")
-dataSheet = data.add_worksheet("HW Submission")
+sheetQuiz = data.add_worksheet("Quiz Submission")
+sheetHW = data.add_worksheet("HW Submission")
 cell_format = data.add_format({'bold': True, 'center_across': True})
 
-# Inputting the headers
-row = 0
-column = 0
-dataSheet.write(row, column, "Student Name", cell_format)
-column = column + 1
-dataSheet.write(row, column, "UF ID", cell_format)
-column = column + 1
-dataSheet.write(row, column, "Canvas ID", cell_format)
-column = column + 1
-for a in assignments:
-    label = a.name.split(':')[0]
-    dataSheet.write(row, column, label, cell_format)
-    column = column + 1
 
-# Writing each person and their associated data
-row = 1
-for person in idToCanvas:
-    col = 0
-    dataSheet.write(row, col, idToCanvas[person]['name'])
-    col = col + 1
-    dataSheet.write_number(row, col, int(idToCanvas[person]['UF']))
-    col = col + 1
-    dataSheet.write_number(row, col, int(idToCanvas[person]['canvas']))
+def createMap(assignments, idStructure):
+    # Searching for all of the homework assignments
     for a in assignments:
-        col = col + 1
+        print(a.name)
+        # Due date of the assignment
         try:
-            dataSheet.write_number(row, col, float(idToCanvas[person][a.name]))
+            due_date = datetime.datetime.strptime(a.due_at, "%Y-%m-%dT%H:%M:%SZ")
+        # Except needed for when there are multiple due dates
         except:
-            dataSheet.write(row, col, "NaN")
-    row = row + 1
+            overrides = a.get_overrides()
+            # Getting the override ID for the assignment
+            for o in overrides:
+                if section_ID in str(o):
+                    num = str(o).split(") (")
+                    override_ID = (num[1][:-1])
+            override = a.get_override(override_ID)
+            due_date = datetime.datetime.strptime(override.due_at, "%Y-%m-%dT%H:%M:%SZ")
+        for s in a.get_submissions(include=["submission_history"]):
+            try:
+                # Only acting on students in specified section
+                for person in idStructure:
+                    if s.user_id == person:
+                        sub_date = datetime.datetime.strptime(s.submitted_at, "%Y-%m-%dT%H:%M:%SZ")
+                        duration = due_date - sub_date
+                        duration_seconds = duration.total_seconds()
+                        hours = duration_seconds / 3600
+                        idStructure[person].update({a.name: str(hours)})
+            except:
+                print("NaN for: " + str(person))
+        print("\n")
+
+
+def dataStorage(assignments, idStructure, sheet):
+    # Inputting the headers
+    row = 0
+    column = 0
+    sheet.write(row, column, "Student Name", cell_format)
+    column = column + 1
+    sheet.write(row, column, "UF ID", cell_format)
+    column = column + 1
+    sheet.write(row, column, "Canvas ID", cell_format)
+    column = column + 1
+    for a in assignments:
+        label = a.name.split(':')[0]
+        sheet.write(row, column, label, cell_format)
+        column = column + 1
+
+    # Writing each person and their associated data
+    row = 1
+    for person in idStructure:
+        col = 0
+        sheet.write(row, col, idStructure[person]['name'])
+        col = col + 1
+        sheet.write_number(row, col, int(idStructure[person]['UF']))
+        col = col + 1
+        sheet.write_number(row, col, int(idStructure[person]['canvas']))
+        for a in assignments:
+            col = col + 1
+            try:
+                sheet.write_number(row, col, float(idStructure[person][a.name]))
+            except:
+                sheet.write(row, col, "NaN")
+        row = row + 1
+
+    sheet.set_column(0, row, 18)
+
+
+# Produce results for quizzes
+createMap(assignmentsQuiz, idQuiz)
+createMap(assignmentsHW, idHW)
+
+# Produce results for homeworks
+dataStorage(assignmentsQuiz, idQuiz, sheetQuiz)
+dataStorage(assignmentsHW, idHW, sheetHW)
 
 # Saving the excel file
-dataSheet.set_column(0, row, 18)
 data.close()
