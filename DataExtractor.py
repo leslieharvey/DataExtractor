3ac3eb691c490d4de6b19948cbce6e01fb0efead
Leslie Harvey <lharvey427@gmail.com>
2019-12-22 21:27:15 -0500

    remove reliance on UFID

--- DataExtractor.py
+++ DataExtractor.py
@@ -9,30 +9,46 @@ courseID = 378337
 section_num = ["000.UFL.2019-08-UF-0.13148"]
 section_ID = "COP2271-29AD(13148)"
 
+num_ID = 547944
 # Initialize a new Canvas object
 canvas = Canvas(API_URL, API_KEY)
 # Get a course object for this course
 course = canvas.get_course(courseID)
+sections = course.get_sections()
+
+# for s in sections:
+#     # print(s.attributes)
+#     print(s.name)
+#     print(s.id)
+#     print("")
+
+enroll = course.get_enrollments(type=["StudentEnrollment"])
+
+enrollments = []
+for e in enroll:
+    if e.course_section_id == num_ID:
+        enrollments.append(e)
+        # print(e.attributes)
 
 # Get students for the specified section
-enrollments = course.get_enrollments(type=["StudentEnrollment"], sis_section_id=section_num)
+# enrollments = course.get_enrollments(type=["StudentEnrollment"], sis_section_id=section_num)
 
 # Build a map of Canvas ID to their student ID using a nested dictionary for HW
 idHW = {}
 for e in enrollments:
-    idHW[e.user['id']] = {'canvas': e.user['id'], 'UF': e.sis_user_id, 'name': e.user['name']}
+    idHW[e.user['id']] = {'canvas': e.user['id'], 'name': e.user['name']}
 assignmentsHW = course.get_assignments(includes=['overrides'], search_term="HW:")
 
 # Build a map of Canvas ID to their student ID using a nested dictionary for Quizzes
 idQuiz = {}
 for e in enrollments:
-    idQuiz[e.user['id']] = {'canvas': e.user['id'], 'UF': e.sis_user_id, 'name': e.user['name']}
+    idQuiz[e.user['id']] = {'canvas': e.user['id'], 'name': e.user['name']}
 assignmentsQuiz = course.get_assignments(includes=['overrides'], search_term="Quiz")
 
 # Build a map of Canvas ID to their student ID using a nested dictionary for Exams
 idExam = {}
 for e in enrollments:
-    idExam[e.user['id']] = {'canvas': e.user['id'], 'UF': e.sis_user_id, 'name': e.user['name']}
+    idExam[e.user['id']] = {'canvas': e.user['id'], 'name': e.user['name']}
 # Ensure that only exams are included in assignments
 groups = course.get_assignment_groups()
 for g in groups:
@@ -49,7 +65,7 @@ assignmentsExam = course.get_assignments(includes=['overrides'], assignment_ids=
 # Build a map of Canvas ID to their student ID using a nested dictionary for Final Grades
 idFinalGrade = {}
 for e in enrollments:
-    idFinalGrade[e.user['id']] = {'canvas': e.user['id'], 'UF': e.sis_user_id, 'name': e.user['name'], 'final_grade': e.grades['current_score']}
+    idFinalGrade[e.user['id']] = {'canvas': e.user['id'], 'name': e.user['name'], 'final_grade': e.grades['current_score']}
 # # Need to figure out how to get past attempt history
 # assignment = course.get_assignment(3970170)
 # submissions = assignment.get_submission(1028980, include="submission_history")
@@ -106,8 +122,6 @@ def dataStorage(assignments, idStructure, sheet):
     column = 0
     sheet.write(row, column, "Student Name", cell_format)
     column = column + 1
-    sheet.write(row, column, "UF ID", cell_format)
-    column = column + 1
     sheet.write(row, column, "Canvas ID", cell_format)
     column = column + 1
     for a in assignments:
@@ -120,8 +134,6 @@ def dataStorage(assignments, idStructure, sheet):
     for person in idStructure:
         col = 0
         sheet.write(row, col, idStructure[person]['name'])
-        col = col + 1
-        sheet.write_number(row, col, int(idStructure[person]['UF']))
         col = col + 1
         sheet.write_number(row, col, int(idStructure[person]['canvas']))
         for a in assignments:
@@ -141,8 +153,6 @@ def dataStorageFinalGrade(idStructure, sheet):
     column = 0
     sheet.write(row, column, "Student Name", cell_format)
     column = column + 1
-    sheet.write(row, column, "UF ID", cell_format)
-    column = column + 1
     sheet.write(row, column, "Canvas ID", cell_format)
     column = column + 1
     sheet.write(row, column, "Final Grade", cell_format)
@@ -151,8 +161,6 @@ def dataStorageFinalGrade(idStructure, sheet):
     for person in idStructure:
         col = 0
         sheet.write(row, col, idStructure[person]['name'])
-        col = col + 1
-        sheet.write_number(row, col, int(idStructure[person]['UF']))
         col = col + 1
         sheet.write_number(row, col, int(idStructure[person]['canvas']))
         col = col + 1
